426058b8f34f4e1f5f5667b28f67a3dacb7bd5dc
Sebastian Juarez <ssebbass@gmail.com>
2015-11-26 23:05:02 -0300

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -xe
 
+cd /tmp
 VERSIONSURL="https://s3.amazonaws.com/Minecraft.Download/versions/versions.json"
 VERSION=$( wget -O - $VERSIONSURL 2>/dev/null | grep latest -2 | grep release | awk '{ print $2 }' | tr -d \" | tr -d '[[:space:]]' )
 wget "https://s3.amazonaws.com/Minecraft.Download/versions/$VERSION/minecraft_server.$VERSION.jar"
@@ -8,4 +9,4 @@ wget "https://s3.amazonaws.com/Minecraft.Download/versions/$VERSION/minecraft_se
 cd /srv
 echo "eula=true" > eula.txt
 
-exec java -Xms512M -Xmx900M -jar /minecraft_server.$VERSION.jar
+exec java -Xms512M -Xmx900M -jar /tmp/minecraft_server.$VERSION.jar
