81435067687c8dd407abd122adc842979a406c6d
Sebastian Juarez <ssebbass@gmail.com>
2015-11-27 13:49:21 -0300

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -3,14 +3,18 @@ set -xe
 
 cd /tmp
 
-# All this should be to get the last version, but I have "problems" whith the client
-# VERSIONSURL="https://s3.amazonaws.com/Minecraft.Download/versions/versions.json"
-# VERSION=$( wget -O - $VERSIONSURL 2>/dev/null | grep latest -2 | grep release | awk '{ print $2 }' | tr -d \" | tr -d '[[:space:]]' )
-# wget "https://s3.amazonaws.com/Minecraft.Download/versions/$VERSION/minecraft_server.$VERSION.jar"
+# Trying to find out what it's the forge recommanded version, and with what minecraft version
+FORGEVER="http://files.minecraftforge.net/maven/net/minecraftforge/forge/promotions_slim.json"
+RECOMMENDEDFORGE=$( wget -O - $FORGEVER 2>/dev/null | grep '"recommended":' | awk '{print $2}' | tr -d \" | tr -d '[[:space:]]' )
+RECOMMENDEDMINE=$( wget -O - $FORGEVER 2>/dev/null | grep $RECOMMENDEDFORGE | grep -v '"recommended"' | awk '{print $1}' | tr -d \" | tr -d \: | tr -d '[[:space:]]' | awk -F \- '{print $1}' )
+FORGEVERSION="$RECOMMENDEDMINE-$RECOMMENDEDFORGE"
 
-wget "https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar"
+# Download forga and minecraft
+wget http://files.minecraftforge.net/maven/net/minecraftforge/forge/$FORGEVERSION/forge-$FORGEVERSION-universal.jar
+wget "https://s3.amazonaws.com/Minecraft.Download/versions/$RECOMMENDEDMINE/minecraft_server.$RECOMMENDEDMINE.jar"
 
+# Go to the data dir and run the forge installer and then the minecraft server
 cd /srv
+exec java -jar /tmp/forge-$FORGEVERSION-universal.jar --installServer
 echo "eula=true" > eula.txt
-
-exec java -Xms512M -Xmx900M -jar /tmp/minecraft_server.1.8.jar
+exec java -Xms512M -Xmx900M -jar /tmp/minecraft_server.$RECOMMENDEDMINE.jar
