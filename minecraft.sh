6325f7d36a0d9e3b28d3e121a8401c3661919e42
Sebastian Juarez <ssebbass@gmail.com>
2015-11-27 13:08:19 -0300

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -2,9 +2,13 @@
 set -xe
 
 cd /tmp
-VERSIONSURL="https://s3.amazonaws.com/Minecraft.Download/versions/versions.json"
-VERSION=$( wget -O - $VERSIONSURL 2>/dev/null | grep latest -2 | grep release | awk '{ print $2 }' | tr -d \" | tr -d '[[:space:]]' )
-wget "https://s3.amazonaws.com/Minecraft.Download/versions/$VERSION/minecraft_server.$VERSION.jar"
+
+# All this should be to get the last version, but I have "problems" whith the client
+# VERSIONSURL="https://s3.amazonaws.com/Minecraft.Download/versions/versions.json"
+# VERSION=$( wget -O - $VERSIONSURL 2>/dev/null | grep latest -2 | grep release | awk '{ print $2 }' | tr -d \" | tr -d '[[:space:]]' )
+# wget "https://s3.amazonaws.com/Minecraft.Download/versions/$VERSION/minecraft_server.$VERSION.jar"
+
+wget "https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar"
 
 cd /srv
 echo "eula=true" > eula.txt
