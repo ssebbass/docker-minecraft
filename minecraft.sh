5952682fb4b67e65fd024dd4d6d283066a6c9d96
Sebastian Juarez <ssebbass@gmail.com>
2015-11-27 13:50:47 -0300

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -14,7 +14,7 @@ wget http://files.minecraftforge.net/maven/net/minecraftforge/forge/$FORGEVERSIO
 wget "https://s3.amazonaws.com/Minecraft.Download/versions/$RECOMMENDEDMINE/minecraft_server.$RECOMMENDEDMINE.jar"
 
 # Go to the data dir and run the forge installer and then the minecraft server
-cd /srv
 exec java -jar /tmp/forge-$FORGEVERSION-universal.jar --installServer
+cd /srv
 echo "eula=true" > eula.txt
 exec java -Xms512M -Xmx900M -jar /tmp/minecraft_server.$RECOMMENDEDMINE.jar
