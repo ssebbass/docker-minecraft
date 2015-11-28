e4c90f08d88015812e7fe0d252e578153ea1f812
Sebastian Juarez <ssebbass@gmail.com>
2015-11-28 13:51:07 -0300

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -71,5 +71,5 @@ fi
 
 java -jar /tmp/forge-$FORGEVERSION-installer.jar --installServer >/dev/null 2>&1
 echo "eula=true" > eula.txt
-tmux new "java -Xms512M -Xmx900M -jar /tmp/minecraft_server.$RECOMMENDEDMINE.jar"
+java -Xms512M -Xmx900M -jar /tmp/minecraft_server.$RECOMMENDEDMINE.jar
 
